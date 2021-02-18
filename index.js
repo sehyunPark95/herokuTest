8399ed517f4e366a5ae32ba1e67234a867a552e5
sehyunPark95 <sh.park@mindwareworks.com>
2021-02-19 02:59:17 +0900

    plz11

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/?city=보험', function (req, res) {
   );
 })
 
-pp.get('/?city=$[ad3268032f7f4e60a453ea5cba049891]', function (req, res) {
+app.get('/?city=$[ad3268032f7f4e60a453ea5cba049891]', function (req, res) {
   //res.send('벌써 열번째');
   res.status(200).json(
     {

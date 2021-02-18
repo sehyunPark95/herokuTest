b444d0ec0ab175bb3bdbdba8415830062d6b5fd4
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-02-18 17:02:19 +0900

    papa

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ app.get('/', function (req, res) {
   res.send('Hello World2222252312321!!');
 })
 
-router.post('/test',controller.postTestAPI)
+router.post('post_test',function(req,res){
+  const user_message = req.body.message;
+  res.status(200).json({
+    "message":user_message
+  })
+});
 
 
 app.listen(app.get('port'), function () {
   console.log('App is running, server is listening on port ', app.get('port'));
 });
+
+module.exports = router;

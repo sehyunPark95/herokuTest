c92e810422a85ab7fc4634431d098703a7e954db
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-02-18 15:39:47 +0900

    four

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 var express = require('express');
 var request = require("request");
 const port = process.env.PORT;
-//서버 슬립안하게
-(function () {  
-    setInterval(function () {
-      var options = {
-        method: 'GET',
-        url:
-          'https://bitpump.herokuapp.com/'
-      };
-  
-      request(options, function (error, response, body) {
-        if (error) {
-          res.send(error)
-        } else {
-          res.send(body);
-        }
-      });
-    }, 600000);
-  })
-///////////////////////
 
 var app = express();
 
@@ -28,5 +9,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(port, ()=> {
-  console.log('App is running, server is listening on port ', app.get('port'));
+  console.log('App is running, server is listening on port ', port);
 });

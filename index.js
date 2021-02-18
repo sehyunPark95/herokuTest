7a9e9415e79c852d76e44ee1a1a4101f08d3054d
sehyunPark95 <sh.park@mindwareworks.com>
2021-02-19 03:02:40 +0900

    plz13

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ var app = express();
 var router = express.Router();
 const controller = require('./module');
 app.set('port', (process.env.PORT || 5000));
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   //res.send('벌써 열번째');
   res.status(200).json(
     {
       "success" : 'qqq'
     }
   );
-})
+})*/
 
 app.get('/?city=보험', function (req, res) {
-  //res.send('벌써 열번째');
+
   res.status(200).json(
     {
       "city" : '23'
@@ -22,7 +22,7 @@ app.get('/?city=보험', function (req, res) {
 })
 
 app.get('/?city=$[ad3268032f7f4e60a453ea5cba049891]', function (req, res) {
-  //res.send('벌써 열번째');
+
   res.status(200).json(
     {
       "city" : '24'

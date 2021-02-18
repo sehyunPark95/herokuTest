518656c760f99829b39a2e6c117653bf4d3c5185
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-02-18 17:28:55 +0900

    plz2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const controller = require('./module');
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
-  res.send('벌써 열번째');
+  //res.send('벌써 열번째');
   res.status(200).json(
     {
       "success" : true

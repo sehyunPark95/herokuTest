761ec74bb29aaf2d382b386d078df8581852eeb7
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-02-18 15:05:41 +0900

    modify

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var request = require("request");
+
+//서버 슬립안하게
+(function () {  
+    setInterval(function () {
+      var options = {
+        method: 'GET',
+        url:
+          'https://bitpump.herokuapp.com/'
+      };
+  
+      request(options, function (error, response, body) {
+        if (error) {
+          res.send(error)
+        } else {
+          res.send(body);
+        }
+      });
+    }, 600000);
+  })
+///////////////////////
+
 var app = express();
 
 app.set('port', (process.env.PORT || 8000));
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.send('Hello World222222');
 })
 
 app.listen(app.get('port'), function () {

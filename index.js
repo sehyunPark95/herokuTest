d9bb78b0479386fbde6fed1aabce42b8c5a79d0f
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-02-18 16:57:31 +0900

    seven

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
-
+var router = express.Router();
+const controller = require('./module');
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
   res.send('Hello World2222252312321!!');
 })
+
+router.post('/test',controller.postTestAPI)
+
 
 app.listen(app.get('port'), function () {
   console.log('App is running, server is listening on port ', app.get('port'));
---
+++ module.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+function basicAPI(req,res){
+    res.status(200).json({
+        "success" :true
+    });
+}
+
+function testAPI(req,res){
+    res.status(200).json({
+        "message":"test"
+    })
+}
+
+function postTestAPI(req,res){
+    const user_message = req.body.message;
+    res.status(200).json({
+        "message": user_message
+    })
+}
+
+
+
+module.exports ={
+    basicAPI : basicAPI,
+    testAPI : testAPI,
+    postTestAPI : postTestAPI
+}

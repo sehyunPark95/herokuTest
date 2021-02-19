da1971de27694d9c524a662adc25623d65594902
sehyunPark95 <sh.park@mindwareworks.com>
2021-02-19 12:41:31 +0900

    plz45

--- index.js
+++ index.js
@@ -12,40 +12,14 @@ app.set('port', (process.env.PORT || 5000));
   );
 })*/
 
-app.get('/?city=보험', function (req, res) {
-
+app.get('/', function (req, res) {
+  const test=  req.query.city;
   res.status(200).json(
     {
-      "city" : '23'
+      "city" : test
     }
   );
 })
-
-app.get('/?city=$[ad3268032f7f4e60a453ea5cba049891]', function (req, res) {
-
-  res.status(200).json(
-    {
-      "city" : '24'
-    }
-  );
-})
-app.post('/?city=보험',function(req,res){
-  
-  console.log(req.body)
-  
-  res.status(200).json({
-    "city": "24"
-  })
-});
-
-app.post('/?city=$[ad3268032f7f4e60a453ea5cba049891]',function(req,res){
-  
-  console.log(req.body)
-  
-  res.status(200).json({
-    "city": "24"
-  })
-});
 
 
 app.post('/test',function(req,res){

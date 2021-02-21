e786b54dcc2eedd9261d4f183ff2564f0b1d05ff
sehyunPark95 <sh.park@mindwareworks.com>
2021-02-21 11:06:35 +0900

    zz

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res) {
 
 app.post('/',function(req,res){
   const test2 = req.body.userInfo;
-  console.log(test2.type)
+  console.log(test2)
   res.status(200).json({
     "message": test2
   })

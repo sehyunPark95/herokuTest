c7701170e70dd9e5390f4c1b0f7c671ee6e2b7d3
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-03-25 10:26:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -263,9 +263,8 @@ app.post('/max',function(req,res){
 //////////////////출금여부//////////////////
 app.post('/maxYn',function(req,res){
   console.log(req)
-  console.log(req.body)
     res.status(200).json( {
-        "valid":"true"
+        "valid":"pass"
     })
   })
 

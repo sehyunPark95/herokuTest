f7d30cb4948ded510bce703a93e2e5b70121d7dc
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-05-07 15:37:03 +0900

    Create index.js

--- index.js
+++ index.js
@@ -1178,7 +1178,7 @@ app.post('/IRP_pdf',function(req,res){
       ]
     })
   })
-//////////////////////////////////////
+//////////////////////////////////////////////////////
 
 ////////////////퇴직연금고객///////////////////////
 app.post('/retiredCustInfo',function(req,res){
@@ -1307,7 +1307,7 @@ app.post('/DC_move',function(req,res){
     })
   })
 app.post('/IRP_move',function(req,res){
-  console.log('-----------퇴직연금 DC_계약이전---------')
+  console.log('-----------퇴직연금 IRP_계약이전---------')
   console.log(req.body.userInfo.query)
   console.log('----------------')
   
@@ -1320,7 +1320,8 @@ app.post('/IRP_move',function(req,res){
       ]
     })
   })
-///////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+///////////////////////고객정보 변경 ///////////////////////////
 app.post('/custInfoChage',function(req,res){
   console.log('-----------퇴직연금 고객정보 변경---------')
   console.log(req.body.userInfo.query)
@@ -1335,7 +1336,7 @@ app.post('/custInfoChage',function(req,res){
       ]
     })
   })
-
+///////////////////////////////////////////////////////////////
 
 app.listen(app.get('port'), function () {
   console.log('App is running, server is listening on port ', app.get('port'));

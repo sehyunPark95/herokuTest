500cd48f232623b9c4d8cee12d009245cccb1bf4
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-06-04 16:03:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2450,7 +2450,7 @@ app.post('/retiring_pension_in',function(req,res){
         },
         {
           "name": "CNTRNUM",
-          "value": "",
+          "value": "null",
           "type": "TEXT",
           "action": "EQ",
           "valueType": "TEXT"

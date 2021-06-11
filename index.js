d4bc769a6e1899fb922cbe24aa2384dbb8104eff
sehyunPark95 <sh.park@mindwareworks.com>
2021-06-11 12:02:07 +0900

    DFAS

--- index.js
+++ index.js
@@ -708,7 +708,7 @@ app.post('/max2',function(req,res){
   }else if(type == '107'){
     console.log('유효성체크')
     res.status(200).json({
-      "data":[{"Error":"Y"}]
+      "data":[{"ERROR":"99"}]
     })
   }
 })

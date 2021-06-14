11dd0c6910ecf432121d2599caf4655b83a6b937
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-06-14 18:51:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1751,12 +1751,6 @@ app.post('/doc',function(req,res){
       "userVariable" : [
         {
           "name": "HOLDTYN",
-          "value": "Y",
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },        {
-          "name": "HOLDTYN",
           "value": "N",
           "type": "TEXT",
           "action": "EQ",
@@ -1788,31 +1782,6 @@ app.post('/doc',function(req,res){
         {
           "name": "PERSONNO",
           "value": "6405302",
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },{
-          "name": "INSUREDNM",
-          "value": "최은빈", 
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },        {
-          "name": "KEY2",
-          "value": "40",
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },{
-          "name": "CUSTNM",
-          "value": "최은빈",
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },
-        {
-          "name": "PERSONNO",
-          "value": "8712232",
           "type": "TEXT",
           "action": "EQ",
           "valueType": "TEXT"
@@ -1880,24 +1849,6 @@ app.post('/doc',function(req,res){
       ],
       "userVariable": [
         {
-          "name": "RSLT",
-          "value": 'Y',
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },        {
-          "name": "RSLT",
-          "value": 'Y',
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },        {
-          "name": "RSLT",
-          "value": 'Y',
-          "type": "TEXT",
-          "action": "EQ",
-          "valueType": "TEXT"
-        },        {
           "name": "RSLT",
           "value": 'Y',
           "type": "TEXT",

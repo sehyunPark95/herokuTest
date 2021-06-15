edd587dcf53b1d79ab43e24faca783605347504a
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-06-15 17:15:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1768,7 +1768,7 @@ app.post('/doc',function(req,res){
       "userVariable": [
         {
           "name": "KEY2",
-          "value": "30",
+          "value": "40",
           "type": "TEXT",
           "action": "EQ",
           "valueType": "TEXT"

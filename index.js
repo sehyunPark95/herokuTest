5e00adfcd154207980d21b1edba5708b265e22e1
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-06-24 10:32:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1005,14 +1005,14 @@ app.post('/HappyCall',function(req,res){
           },
           {
             "name": "QUESTCD4",
-            "value": "200301",
+            "value": "300902",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD5",
-            "value": "300903",
+            "value": "300905",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"

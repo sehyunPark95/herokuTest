4f7ae4f8e6a77c19ad7bda2cbe271ae6ea410710
sehyunPark95 <79242568+sehyunPark95@users.noreply.github.com>
2021-06-25 16:43:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1005,56 +1005,56 @@ app.post('/HappyCall',function(req,res){
           },
           {
             "name": "QUESTCD4",
-            "value": "300909",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD5",
-            "value": "300909",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD6",
-            "value": "300909",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD7",
-            "value": "300909",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD8",
-            "value": "300909",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD9",
-            "value": "300909",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD10",
-            "value": "200702",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
           },
           {
             "name": "QUESTCD11",
-            "value": "200702",
+            "value": "301303",
             "type": "TEXT",
             "action": "EQ",
             "valueType": "TEXT"
